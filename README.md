7d038a3523c36824a05ecd91e8ea11aa11e5898f
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2014-11-24 20:32:32 -0700

    Uploaded from alliance auth to seperate repo

---
+++ README.md
@@ -0,0 +1,1 @@
+To utilize this module copy the folder "user_service" to your interface/board/modules/ directory. 
---
+++ user_service/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Remote API User Administration configuration
+ * 
+ * @author 		Author: Raynaldo Rivera 
+ */
+
+$CONFIG['api_module_title'] = 'Userservice';
+$CONFIG['api_module_desc']  = 'Userservice Module';
+$CONFIG['api_module_key']   = 'user_service';
